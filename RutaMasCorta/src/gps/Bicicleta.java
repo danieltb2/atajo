67982e39a8888a8969b7c4a936b20684bc6c312e
ALU1J <ALU1J@PC306>
2020-02-03 10:06:12 +0100

    asdf

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -13,18 +13,27 @@ public class Bicicleta {
         String marca = "Orbea";
     }
     
+    public static int  calculo(int [] arr){
+    	int  sum1=0;
+    	for(int i=0; i<arr.length; i++)
+        {
+           sum1 += arr[i];
+        }
+    	return sum1;
+    }
+    
     public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
     {
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
+        int sum1 = calculo(camino1);
+        int sum2 = calculo(camino2);
+        int sum3 = calculo(camino3);
         
-        for(int i=0; i<camino1.length; i++)
+        /*for(int i=0; i<camino1.length; i++)
         {
             sum1 += camino1[i];
             sum2 += camino2[i];
             sum3 += camino3[i];
-        }
+        }*/
         
         if(sum1 < sum2 && sum1 < sum3)
         {
@@ -53,9 +62,9 @@ public class Bicicleta {
 
     public static void main(String[] args) {
         Bicicleta bici = new Bicicleta();
-        int[] camino1 = {12,34,21,46,25};
-        int[] camino2 = {24,1,5,64,10,15,21};
-        int[] camino3 = {1,5,6,7,8,10,20,46,104};
+        int[] camino1 = {0,0,0,0,3};
+        int[] camino2 = {0,0,0,0,0,0,6};
+        int[] camino3 = {0,0,0,0,0,0,0,0,2};
         System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
     }
 }
